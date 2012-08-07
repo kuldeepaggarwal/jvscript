cb9bd0a54fc977df7ba7a8ed6d7346ff0392a63a
kuldeepaggarwal <kuldeep.aggarwal@vinsol.com>
2012-08-08 01:44:03 +0530

    finally complete

--- ArithmeticQuiz/arithmetic_quiz.js
+++ ArithmeticQuiz/arithmetic_quiz.js
@@ -2,45 +2,43 @@ function start_quiz() {
     window.open('arithmetic_quiz_questions.html', 'TEST WINDOW', 'width=1000,height=400,location=0;scrollbars=0,menubar=0,toolbar=0,resizeable=0,status=0', 1).focus();
 
 }
-var timer_forques, QuesAnsRecord = {};
+var timer_for_ques, QuesAnsRecord = {};
 var input_tb = document.getElementById("answer");
 QuesAnsRecord["score"] = {"correct" : 0, "incorrect" : 0, "unmarked" : 0};
-TotalQues = 20;
-no_of_ques = TotalQues;
-no_correctanswers = 0;
-ClockTimer = 30;
+TotalQues = 20;//Total No of ques
+ques_no = 1;//set for first question
+ClockTimer = 30;//No of seconds for each question
 function timer_display() {
-    document.getElementById('CLK').innerHTML = timer_forques + "s";
-    timer_forques--;
-    setting_time_for_each_ques = setTimeout('timer_display()', 1000);
-    if (timer_forques < -1) {
-        timer_forques = ClockTimer;
-        clearTimeout(setting_time_for_each_ques);
+    if (timer_for_ques == 0) {
+      resetTimer();
+    } else {
+      timer_for_ques--;
+      document.getElementById('CLK').innerHTML = timer_for_ques + "s";
     }
 }
-function clock() {
-    CreateQuestions(no_of_ques);
-    document.getElementById("next").disabled =false;
-    setting_time_for_each_ques = setTimeout('timer_display()', 0);//just for initialisation of 'setting_time_for_each_ques'
-    set_interval_for_next_ques = setInterval('next_question()', 0);
+function startTest() {
+    GenerateQuestions(TotalQues);
+    setTimer();
 }
-
+function setTimer() {
+  timer_for_ques = ClockTimer;
+  document.getElementById('CLK').innerHTML = ClockTimer + "s";
+  setting_time_for_each_ques = setInterval('timer_display()',1000)
+  next_question();
+}
+function resetTimer() {
+  clearInterval(setting_time_for_each_ques);
+  evaluation(ques_no);
+  ques_no++;
+  empty_answer_tb();
+  setTimer();
+}
 function next_question() {
-    clearTimeout(setting_time_for_each_ques);
-    no_of_ques--;
-    clearInterval(set_interval_for_next_ques);
-    document.getElementById('CLK').innerHTML = timer_forques + "s";
-    if (no_of_ques != 19) {
-        evaluation();
-        empty_answer_tb();
-    }
-    if (no_of_ques <= -1) {
+    if (ques_no == TotalQues) {
+        evaluation(ques_no);
         Result(TotalQues);        
     } else {
-        question_printing(20 - Number(no_of_ques));
-        set_interval_for_next_ques = setInterval('next_question()', 30500);
-        timer_display();
-        timer_forques = ClockTimer;
+        question_printing(ques_no);
     }
 }
 
@@ -48,34 +46,43 @@ function question_printing(ques_no) {
     document.getElementById("1").textContent = QuesAnsRecord[ques_no]["question"];
     document.getElementById("ques_num_lbl").textContent = ques_no + ":";
 }
-function CreateQuestions(NoOfQues) {
+function GenerateQuestions(NoOfQues) {
   for(var i = 1; i <= NoOfQues; i++) {
-    var num1 = Math.floor((Math.random() * 20) + 1), num2 = Math.floor((Math.random() * 20) + 1);
-    operator = Math.floor((Math.random() * 10) / 2);
-    switch (operator) {
-    case Number(1):
+    var num1 = numGenerate(), num2 = numGenerate();
+    operator =  genOper();
+    operation(num1, num2, i ,operator);
+  }
+}
+function numGenerate() {
+  return Math.floor((Math.random() * 20) + 1);
+}
+function genOper() {
+  return Math.floor((Math.random() * 4)) + 0;
+}
+function operation(num1, num2, i,operator) {
+    operators = ['+','-','*','/']
+    switch (operators[operator]) {
+    case '+':
         QuesAnsRecord[i] = {"question" : num1 + ' + ' + num2, "expected_ans" : eval(num1 + num2), "user_ans" : ""};
         break;
-    case Number(2):
+    case '-':
         QuesAnsRecord[i] = {"question" : num1 + ' - ' + num2, "expected_ans" : eval(num1 - num2), "user_ans" : ""};
         break;
-    case Number(3):
+    case '*':
         QuesAnsRecord[i] = {"question" : num1 + ' * ' + num2, "expected_ans" : eval(num1 * num2), "user_ans" : ""};
         break;
-    case Number(4):
+    case '/':
         QuesAnsRecord[i] = {"question" : num1 + ' / ' + num2, "expected_ans" : Math.floor((num1 / num2) * 100) / 100, "user_ans" : ""};
         break;
     default:
-        QuesAnsRecord[i] = {"question" : num1 + ' * ' + num2, "expected_ans" : eval(num1 * num2), "user_ans" : ""};
+        console.log("Error");
     }
-  }
 }
-function evaluation() {
-    var previous_qtn_num = 20 - Number(no_of_ques) - 1;
-    QuesAnsRecord[previous_qtn_num]["user_ans"]= parseFloat(input_tb.value) ;
-    if (isNaN(QuesAnsRecord[previous_qtn_num]["user_ans"])) {
+function evaluation(ques) {
+    QuesAnsRecord[ques]["user_ans"]= parseFloat(input_tb.value) ;
+    if (isNaN(QuesAnsRecord[ques]["user_ans"])) {
         QuesAnsRecord["score"]["unmarked"] += 1;
-    } else if (QuesAnsRecord[previous_qtn_num]["user_ans"] == QuesAnsRecord[previous_qtn_num]["expected_ans"]) {
+    } else if (QuesAnsRecord[ques]["user_ans"] == QuesAnsRecord[ques]["expected_ans"]) {
          QuesAnsRecord["score"]["correct"] += 1;
     } else {
         QuesAnsRecord["score"]["incorrect"] += 1;
@@ -108,8 +115,6 @@ function Result(NoOfQues) {
     }
     tablerow.style.backgroundColor = bgcolor;
     tablerow.style.color = forecolor;
-    console.log(tablerow.style.textColor);
-    console.log(tablerow.style.color);
     }
     document.getElementById("total_score").innerText = QuesAnsRecord["score"]["correct"];
 }
